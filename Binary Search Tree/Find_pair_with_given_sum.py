dbd6e1477e29d6599c49ef4b85c345934cf8e931
Komal <komalbansal97@gmail.com>
2020-09-04 23:02:26 +0530

    find pair with given sum in bst updated

--- Binary Search Tree/Find_pair_with_given_sum.py
+++ Binary Search Tree/Find_pair_with_given_sum.py
@@ -12,7 +12,8 @@ Output:
 0
 '''
 
-# method - 1, hashing based solution
+# method - 1, hashing based solution using DFS(recursive) -> O(n) space
+# method - 2, hashing based solution using BFS(iterative) -> levelorder ->  O(n)space
 def isPairPresent(root, target): 
     
     m = {}
@@ -29,3 +30,44 @@ def isPairPresent(root, target):
         right = check(root.right, target)
         return left or right
     return int(check(root, target))
+
+# method - 3 => logic is same of 2 pointer approach to find pair in sorted array
+# do inorder traversal and inverse inorder traversal, do this iterative using stack - O(log n) space
+class Solution:
+       
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        if root == None or (root.left == None and root.right == None):
+            return False
+        
+        leftinorder = []
+        rightinorder = []
+        
+        leftptr = root
+        rightptr = root
+        
+        while leftptr != None:
+            leftinorder.append(leftptr)
+            leftptr = leftptr.left
+        
+        while rightptr != None:
+            rightinorder.append(rightptr)
+            rightptr = rightptr.right
+            
+        while len(leftinorder) > 0 and len(rightinorder) > 0:
+            
+            if (leftinorder[-1].val + rightinorder[-1].val) == k and leftinorder[-1] != rightinorder[-1]:
+                return True
+            
+            elif (leftinorder[-1].val + rightinorder[-1].val) < k:
+                leftptr = leftinorder.pop()
+                leftptr = leftptr.right
+                while leftptr != None:
+                    leftinorder.append(leftptr)
+                    leftptr = leftptr.left
+            else:
+                rightptr = rightinorder.pop()
+                rightptr = rightptr.left
+                while rightptr != None:
+                    rightinorder.append(rightptr)
+                    rightptr = rightptr.right
+        return False
